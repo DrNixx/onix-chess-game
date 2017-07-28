248844f04f66264559313ce3ba5cbdf5842e0bf6
Nick Panteleeff <drnix@chess-online.com>
2017-07-28 14:45:11 +0300

    Add export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export { ViewGame } from './chess/ViewGame';
+import * as GameActionTypes from './chess/GameActionTypes';
+import * as GameActionConsts from './chess/GameActionConsts';
+import * as GameActions from './chess/GameActions';
+
+export { ViewGame, ChessGameProps } from './chess/ViewGame';
+export { registerStrings } from './Intl';
+
+export { PlayStore } from './chess/GameStore';
+export { gameReducer } from './chess/GameReducer';
+
+export { GameActionTypes, GameActionConsts, GameActions };
